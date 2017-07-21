f5caf6e471511771cac933558e00a6f90983e2a9
rileymills <rtmills@deakin.edu.au>
2017-07-21 20:59:05 +1000

    Initial check-in of module CalendarApp

---
+++ CalendarApp/CalendarAppPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+using XamForms.Controls;
+
+namespace CalendarApp
+{
+    public partial class CalendarAppPage : ContentPage
+    {
+        public CalendarAppPage()
+        {
+            InitializeComponent();
+
+			Calendar calendar = new Calendar()
+			{
+				WidthRequest = 300,
+				HeightRequest = 300
+			};
+
+			calendar.DateClicked += (object sender, DateTimeEventArgs e) =>
+			{
+				var dateSelect = calendar.SelectedDate;
+			};
+		}
+    }
+}
