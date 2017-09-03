ca9ab60970d2739bae8d76145b234803215a43d4
rileymills <rtmills@deakin.edu.au>
2017-09-03 15:49:55 +1000

    Add HTML post and get requests for username and password, so they are stored on an online datastore. Add some validation for empty fields.

--- CalendarApp/LoginPage.xaml.cs
+++ CalendarApp/LoginPage.xaml.cs
@@ -2,17 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security;
+using System.IO;
+using System.Net.Http;
+using System.Text;
 
 using Xamarin.Forms;
+
 
 namespace CalendarApp
 {
     public partial class LoginPage : ContentPage
     {
+		public async void RetrieveLogin()
+		{
+            if (userInput != null)
+            {
+                var httpClient = new HttpClient();
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://introtoapps.com/datastore.php?action=load&appid=215075797&objectid="+userInput);
+                var response = await httpClient.SendAsync(request);
+            }
+
+            if(passwordInput != null){
+				var httpClient = new HttpClient();
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://introtoapps.com/datastore.php?action=load&appid=215075797&objectid=" + passwordInput);
+				var response = await httpClient.SendAsync(request);
+            } 
+		}
+
+		public async void SendLogin()
+		{
+            if (userInput != null)
+            {
+                var httpClient = new HttpClient();
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "http://introtoapps.com/datastore.php?action=save&appid=215075797&objectid=" + userInput + "&data=" + userInput);
+                var response = await httpClient.SendAsync(request);
+            }
+		}
+
+		public async void SendPassword()
+		{
+            if (passwordInput != null)
+            {
+                var httpClient = new HttpClient();
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "http://introtoapps.com/datastore.php?action=save&appid=215075797&objectid=" + passwordInput + "&data=" + passwordInput);
+                var response = await httpClient.SendAsync(request);
+            }
+		}
+
+
+        public string userInput { get; set; }
+
+        public string passwordInput { get; set; }
+
+
         public LoginPage()
         {
             InitializeComponent();
-
 
 
 			//Heading for the page
@@ -25,7 +70,19 @@ namespace CalendarApp
 
             var usernameEntry = new Entry { Placeholder = "User Name" };
 
+            usernameEntry.TextChanged += OnUserNameEntry;
+            void OnUserNameEntry(object sender, EventArgs e){
+                userInput = usernameEntry.Text;
+            }
+
+
             var passwordEntry = new Entry { Placeholder = "Password", IsPassword = true };
+
+            passwordEntry.TextChanged += OnPasswordEntry;
+            void OnPasswordEntry(object sender, EventArgs e){
+                passwordInput = passwordEntry.Text;
+            }
+
 
 			Button loginButton = new Button
 			{
@@ -54,17 +111,40 @@ namespace CalendarApp
 				BorderColor = Color.FromHex("006996")
 
 			};
+            registerButton.Clicked += OnRegisterButtonClicked;
+			void OnRegisterButtonClicked(object sender, EventArgs e)
+			{
+                if (userInput != null)
+                {
+                    if (passwordInput != null)
+                    {
+                        SendLogin();
+                        SendPassword();
+                        DisplayAlert("Alert", "Account created", "OK");
+                    } 
+                } else { DisplayAlert("Alert", "Account creation failed", "OK");  }
+
+			}
+
 			loginButton.Clicked += OnLoginButtonClicked;
 			void OnLoginButtonClicked(object sender, EventArgs e)
 			{
+                if (userInput != null)
+                {
+                    if (passwordInput != null)
 
-                Navigation.PushAsync(new HomePage());
+                    {
+                        RetrieveLogin();
+                        Navigation.PushAsync(new HomePage());
 
-				var existingPages = Navigation.NavigationStack.ToList();
-				foreach (var page in existingPages)
-				{
-					Navigation.RemovePage(page);
-				}
+                        var existingPages = Navigation.NavigationStack.ToList();
+                        foreach (var page in existingPages)
+                        {
+                            Navigation.RemovePage(page);
+                        }
+                    }
+                }
+                else { DisplayAlert("Alert", "Login failed", "OK"); }
 
 			}
 
@@ -78,17 +158,13 @@ namespace CalendarApp
                     header,
                     usernameEntry,
                     passwordEntry,
-					loginButton
+					loginButton,
+                    registerButton
 
 				}
 			};
 
-
-
         }
-
-
-
 
     }
 }
