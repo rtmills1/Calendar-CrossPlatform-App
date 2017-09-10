30d540f57864f7edf89eaf8f16704d011592630c
rileymills <rtmills@deakin.edu.au>
2017-09-10 19:14:55 +1000

    Added SHA512 hashing to android project.

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,7 +7,12 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using static CalendarApp.LoginPage;
+using System.Security.Cryptography;
+using System.Text;
+using static CalendarApp.Droid.MainActivity;
 
+[assembly: Xamarin.Forms.Dependency(typeof(SHA512StringHasImplementation))]
 namespace CalendarApp.Droid
 {
     [Activity(Label = "CalendarApp.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -24,5 +29,29 @@ namespace CalendarApp.Droid
 
             LoadApplication(new App());
         }
+
+		public class SHA512StringHasImplementation : SHA512StringHash
+		{
+			public SHA512StringHasImplementation() { }
+
+			public string Hash(string input)
+			{
+				SHA512 shaM = new SHA512Managed();
+				// Convert the input string to a byte array and compute the hash.
+				byte[] data = shaM.ComputeHash(Encoding.UTF8.GetBytes(input));
+				// Create a new Stringbuilder to collect the bytes
+				// and create a string.
+				StringBuilder sBuilder = new StringBuilder();
+				// Loop through each byte of the hashed data 
+				// and format each one as a hexadecimal string.
+				for (int i = 0; i < data.Length; i++)
+				{
+					sBuilder.Append(data[i].ToString("x2"));
+				}
+				// Return the hexadecimal string.
+				input = sBuilder.ToString();
+				return (input);
+			}
+		}
     }
 }
